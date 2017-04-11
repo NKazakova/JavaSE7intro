09bb0beea201b46a4751fa4630bb1e6ea749c0e3
marselmax <marselmax@gmail.com>
2017-04-11 15:18:58 +0300

    uytuytyugh
    Signed-off-by: marselmax <marselmax@gmail.com>

---
+++ src/Helloworld.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 11.04.2017.
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        MessageGen messageGen = new MessageGen();
+//;lkjkjn
+
+        System.out.println(messageGen.getMessage());
+    }
+
+    //private static String getMessage() {
+      //  return "Hello world";
+   // }
+}
---
+++ src/MessageGen.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 11.04.2017.
+ */
+public class MessageGen {
+
+    public static String getMessage() {
+        return "Hello world";
+
+   // public static void main(String[] args) {
+
+    }
+}
