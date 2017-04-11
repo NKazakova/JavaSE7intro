4b68d0064868efba225ce1998f725d344063136c
marselmax <marselmax@gmail.com>
2017-04-11 17:07:08 +0300

    uytuytyugh
    Signed-off-by: marselmax <marselmax@gmail.com>

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+/**
+ * Created by user on 11.04.2017.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Person().getAge());
+
+
+    }
+}
