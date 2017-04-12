af8d54c9ace45d00820b873ceeff72f53bce1261
marselmax <marselmax@gmail.com>
2017-04-12 13:50:04 +0300

    1204
    Signed-off-by: marselmax <marselmax@gmail.com>

---
+++ src/MutableTest.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+/**
+ * Created by user on 12.04.2017.
+ */
+public class MutableTest {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        changeDate (date);
+        System.out.println(date);
+    }
+
+    private static void changeDate (Date date){
+        date.setTime(0);}
+}
