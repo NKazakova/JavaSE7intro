d263220d8391d5a4794713d7b21baa6d743d7898
marselmax <marselmax@gmail.com>
2017-04-12 09:37:42 +0300

    1204
    Signed-off-by: marselmax <marselmax@gmail.com>

--- src/Person.java
+++ src/Person.java
@@ -10,7 +10,15 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Person().getAge());
+
+        Person person1 = new Person();
+        person1.age = 10;
+
+        Person person2 = new Person();
+        person2.age = 33;
+
+        System.out.println(person1.getAge());
+        System.out.println(person2.getAge());
 
 
     }
