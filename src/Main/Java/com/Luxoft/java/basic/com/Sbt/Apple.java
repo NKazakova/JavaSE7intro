063f44485ca9d30d38dfe897d1362f90af067cec
sbt-kazakova-nv <kazakova-nata@yandex.ru>
2017-04-27 17:40:52 +0400

    h

---
+++ src/Main/Java/com/Luxoft/java/basic/com/Sbt/Apple.java
@@ -0,0 +1,7 @@
+package com.Luxoft.java.basic.com.Sbt;
+
+/**
+ * Created by SBT-Kazakova-NV on 27.04.2017.
+ */
+public class Apple {
+}
---
+++ src/Main/Java/com/Luxoft/java/basic/com/Sbt/Starter.java
@@ -0,0 +1,13 @@
+package com.Luxoft.java.basic.com.Sbt;
+
+import java.util.List;
+
+/**
+ * Created by SBT-Kazakova-NV on 27.04.2017.
+ */
+public class Starter {
+    public static void main(String[] args) {
+
+    }
+    public static List <Apple>
+}
